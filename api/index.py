28f42745a04a6562813ce2baa98849af748bbc18
fukudahiroto21 <hiroto082100@gmail.com>
2025-12-24 16:35:26 +0900

    Refactor handler to use BaseHTTPRequestHandler

--- api/index.py
+++ api/index.py
@@ -1,2 +1,8 @@
-def handler(request):
-    return "Hello from Vercel + Python"
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"Hello from Vercel + Python")
