53c27472022062b01a68f3921cb15523939637b1
fukudahiroto21 <hiroto082100@gmail.com>
2025-12-24 16:30:47 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,8 @@
 def handler(request):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain; charset=utf-8"
+        },
         "body": "Hello from Vercel + Python"
     }
