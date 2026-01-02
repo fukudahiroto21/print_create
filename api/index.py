3edbee5094ad5642a1c76d892ad8795551d48fd3
fukudahiroto21 <hiroto082100@gmail.com>
2026-01-02 22:48:01 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -34,7 +34,7 @@ def generate_pdf():
 
     # ===== 日本語フォント =====
     pdfmetrics.registerFont(TTFont("IPAexGothic", font_path))
-    c.setFont("IPAexGothic", 42)
+    c.setFont("IPAexGothic", 40)
 
     # ===== 問題生成（10問） =====
     problems = [
